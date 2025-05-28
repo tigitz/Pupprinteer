2fc81564128285d3af2deae0a651a95e6aa84d02
Philippe S <p.segatori@keesystem.com>
2025-05-28 21:57:14 +0200

    style(lint): ignore node/prefer-global/process

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-}, {
+export default antfu({}, {
   files: ['examples/gold-tweet.js'],
+}, {
   rules: {
     'no-console': 'off',
+    'node/prefer-global/process': 'off',
   },
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { Command } from '@commander-js/extra-typings'
-import { buildDebugCommand } from './command/debug'
 import { addDefaultOptions } from './command/global.ts'
 import { buildPdfCommand } from './command/pdf'
 import { buildScreenshotCommand } from './command/screenshot'
@@ -12,13 +11,9 @@ const program = new Command()
 addDefaultOptions(program)
   .addCommand(buildScreenshotCommand())
   .addCommand(buildPdfCommand())
-  .addCommand(buildDebugCommand())
 
 async function main(): Promise<void> {
   await program.parseAsync()
 }
 
-main().catch((error) => {
-  console.error('Error:', error)
-  process.exit(1)
-})
+main()
